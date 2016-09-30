e666e8b5b40e030a28ab386b74b444e4be2e3e4f
Mika Tuupola <tuupola@appelsiini.net>
2016-10-01 01:29:57 +0300

    First working concept

---
+++ README.md
@@ -0,0 +1,47 @@
+# Instrument Middleware
+
+[![Latest Version](https://img.shields.io/packagist/v/tuupola/instrument-middleware.svg?style=flat-square)](https://packagist.org/packages/tuupola/instrument-middleware)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
+[![Build Status](https://img.shields.io/travis/tuupola/instrument-middleware/master.svg?style=flat-square)](https://travis-ci.org/tuupola/instrument-middleware)
+[![HHVM Status](https://img.shields.io/hhvm/tuupola/instrument-middleware.svg?style=flat-square)](http://hhvm.h4cc.de/package/tuupola/instrument-middleware)
+[![Coverage](http://img.shields.io/codecov/c/github/tuupola/instrument-middleware.svg?style=flat-square)](https://codecov.io/github/tuupola/instrument-middleware)
+
+## Install
+
+Install using [composer](https://getcomposer.org/).
+
+``` bash
+$ composer require tuupola/instrument-middleware
+```
+
+## Usage
+
+Here be dragons...
+
+## Testing
+
+You can run tests either manually...
+
+``` bash
+$ vendor/bin/phpunit
+$ vendor/bin/phpcs --standard=PSR2 src/ -p
+```
+
+... or automatically on every code change.
+
+``` bash
+$ npm install
+$ grunt watch
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Security
+
+If you discover any security related issues, please email tuupola@appelsiini.net instead of using the issue tracker.
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/Instrument.php
@@ -0,0 +1,248 @@
+<?php
+
+/*
+ * This file is part of the Instrument middleware package
+ *
+ * Copyright (c) 2016 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/instrument-middleware
+ *
+ */
+
+namespace Tuupola\Middleware;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+class Instrument
+{
+    //use \Witchcraft\Hydrate;
+    use \Witchcraft\MagicMethods;
+    use \Witchcraft\MagicProperties;
+
+    private $options = [
+        "measurement" => "instrument",
+        "bootstrap" => "bootstrap",
+        "total" => "total",
+        "memory" => "memory",
+        "status" => "status",
+        "route" => "route",
+        "tags" => []
+    ];
+
+    private $settings;
+    private $instrument = null;
+
+    protected $logger;
+
+    public function __construct($options)
+    {
+        /* Store passed in options overwriting any defaults. */
+        $this->hydrate($options);
+
+        if ($this->instrument) {
+            //$this->instrument->tags($this->tags());
+        }
+    }
+
+    public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        /* REQUEST_TIME_FLOAT is closer to truth. */
+        if (isset($_SERVER["REQUEST_TIME_FLOAT"])) {
+            $start = $_SERVER["REQUEST_TIME_FLOAT"];
+        } else {
+            $start = microtime(true);
+        }
+
+        $timing = $this->instrument->timing($this->measurement);
+
+        /* Time spent from starting the request to entering first middleware. */
+        $bootstrap = (microtime(true) - $start) * 1000;
+        $timing->set($this->bootstrap, (integer)$bootstrap);
+
+        /* Call all the other middlewares. */
+        $timing->start("process");
+        $response = $next($request, $response);
+        $timing->stop("process");
+
+        /* Store PHP memory usage. */
+        $timing->set($this->memory, (integer)$timing->memory());
+
+        /* Time spent from starting the request to exiting last middleware. */
+        $total = (microtime(true) - $start) * 1000;
+        $this
+            ->instrument
+            ->timing($this->measurement)
+            ->set($this->total, (integer)$total);
+
+        $this->instrument->send();
+        //return $response->write(print_r($this->instrument, true));
+        return $response;
+    }
+
+    /**
+     * Hydate options from given array
+     *
+     * @param array $data Array of options.
+     * @return self
+     */
+    private function hydrate(array $data = [])
+    {
+        foreach ($data as $key => $value) {
+            /* https://github.com/facebook/hhvm/issues/6368 */
+            $key = str_replace(".", " ", $key);
+            $method = "set" . ucwords($key);
+            $method = str_replace(" ", "", $method);
+            if (method_exists($this, $method)) {
+                call_user_func([$this, $method], $value);
+            }
+        }
+        return $this;
+    }
+
+    public function setInstrument($instrument)
+    {
+        $this->instrument = $instrument;
+        return $this;
+    }
+
+    public function setMeasurement($measurement)
+    {
+        $this->measurement = $measurement;
+        return $this;
+    }
+
+    public function getMeasurement()
+    {
+        return $this->options["measurement"];
+    }
+
+    public function setBootstrap($bootstrap)
+    {
+        $this->options["bootstrap"] = $bootstrap;
+        return $this;
+    }
+
+        public function getBootstrap()
+    {
+        return $this->options["bootstrap"];
+    }
+
+    public function setTotal($total)
+    {
+        $this->options["total"] = $total;
+        return $this;
+    }
+
+    public function getTotal()
+    {
+        return $this->options["total"];
+    }
+
+    public function setMemory($memory)
+    {
+        $this->options["memory"] = $memory;
+        return $this;
+    }
+
+    public function getMemory()
+    {
+        return $this->options["memory"];
+    }
+
+    public function setStatus($status)
+    {
+        $this->options["status"] = $statud;
+        return $this;
+    }
+
+    public function getStatus()
+    {
+        return $this->options["status"];
+    }
+
+    public function setRoute($route)
+    {
+        $this->options["route"] = $route;
+        return $this;
+    }
+
+    public function getRoute()
+    {
+        return $this->options["route"];
+    }
+
+    public function setTags(array $tags)
+    {
+        $this->options["tags"] = $tags;
+        return $this;
+    }
+
+    public function getTags()
+    {
+        return $this->options["tags"];
+    }
+
+    /**
+     * Set the logger
+     *
+     * @return self
+     */
+    public function setLogger(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger;
+        return $this;
+    }
+
+    /**
+     * Get the logger
+     *
+     * @return Psr\Log\LoggerInterface
+     */
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+
+    /**
+     * Get the error handler
+     *
+     * @return string
+     */
+    public function getError()
+    {
+        return $this->options["error"];
+    }
+
+    /**
+     * Set the error handler
+     *
+     * @return self
+     */
+    public function setError($error)
+    {
+        $this->options["error"] = $error;
+        return $this;
+    }
+
+    /**
+     * Call the error handler if it exists
+     *
+     * @return Psr\Http\Message\ResponseInterface
+     */
+    public function error(RequestInterface $request, ResponseInterface $response, $arguments)
+    {
+        if (is_callable($this->options["error"])) {
+            $handler_response = $this->options["error"]($request, $response, $arguments);
+            if (is_a($handler_response, "\Psr\Http\Message\ResponseInterface")) {
+                return $handler_response;
+            }
+        }
+        return $response;
+    }
+}
